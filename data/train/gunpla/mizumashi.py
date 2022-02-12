eec96899eea536f5e4e8c683e7ca16afc9980910
StupidGame <matsuo877@gmail.com>
2022-02-12 20:43:19 +0900

    色々削除したり変えたりした

--- data/train/gunpla/mizumashi.py
+++ data/train/gunpla/mizumashi.py
@@ -1,43 +1,95 @@
-import os
-import glob
+from email.mime import base
+import os, glob
+from PIL import Image
 import numpy as np
-from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array, array_to_img
+import uuid
+import sys
+import cv2
+import matplotlib.pyplot as plt
+import test2
 
-def draw_images(generator, x, dir_name, index):
-    # 出力ファイルの設定
-    save_name = 'extened-' + str(index)
-    g = generator.flow(x, batch_size=1, save_to_dir=output_dir, save_prefix=save_name, save_format='jpg')
+def remove_bg(
+    path,
+    BLUR = 21,
+    CANNY_THRESH_1 = 10,
+    CANNY_THRESH_2 = 200,
+    MASK_DILATE_ITER = 10,
+    MASK_ERODE_ITER = 10,
+    MASK_COLOR = (0.0,0.0,1.0),
+):
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    # 1つの入力画像から何枚拡張するかを指定
-    # g.next()の回数分拡張される
-    for i in range(10):
-        bach = g.next()
+    edges = cv2.Canny(gray, CANNY_THRESH_1, CANNY_THRESH_2)
+    edges = cv2.dilate(edges, None)
+    edges = cv2.erode(edges, None)
 
+    contour_info = []
+    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    for c in contours:
+        contour_info.append((
+            c,
+            cv2.isContourConvex(c),
+            cv2.contourArea(c),
+        ))
+    contour_info = sorted(contour_info, key=lambda c: c[2], reverse=True)
+    max_contour = contour_info[0]
+
+    mask = np.zeros(edges.shape)
+    cv2.fillConvexPoly(mask, max_contour[0], (255))
+
+    mask = cv2.dilate(mask, None, iterations=MASK_DILATE_ITER)
+    mask = cv2.erode(mask, None, iterations=MASK_ERODE_ITER)
+    mask = cv2.GaussianBlur(mask, (BLUR, BLUR), 0)
+    mask_stack = np.dstack([mask]*3)    # Create 3-channel alpha mask
+
+    mask_stack  = mask_stack.astype('float32') / 255.0          # Use float matrices, 
+    img         = img.astype('float32') / 255.0                 #  for easy blending
+
+    masked = (mask_stack * img) + ((1-mask_stack) * MASK_COLOR) # Blend
+    masked = (masked * 255).astype('uint8')                     # Convert back to 8-bit 
+
+    c_blue, c_green, c_red = cv2.split(img)
+
+    img_a = cv2.merge((c_red, c_green, c_blue, mask.astype('float32') / 255.0))
+
+    return img_a
+
+def main():
+    filepath_list = glob.glob(input_path + '/*.jpg') # .pngファイルをリストで取得する
+    for filepath in filepath_list:
+        img_fin = remove_bg(    
+            path = filepath,
+            BLUR = 21,
+            CANNY_THRESH_1 = 5,
+            CANNY_THRESH_2 = 70,
+            MASK_DILATE_ITER = 10,
+            MASK_ERODE_ITER = 6,
+        )
+        fig = plt.figure(frameon=False)
+        ax = plt.Axes(fig, [0., 0., 1., 1.])
+        ax.set_axis_off()
+        fig.add_axes(ax)
+        ax = plt.Axes(fig, [0., 0., 1., 1.])
+        ax.set_axis_off()
+        fig.add_axes(ax)
+        ax.imshow(img_fin)
+        fig.savefig(filepath)
+        for i in range(0, 36):
+            uuidgen = str(uuid.uuid4())
+            basename  = os.path.basename(filepath) # ファイルパスからファイル名を取得
+            save_filepath = out_path + '/' + basename [:-4] + '-' + uuidgen + '.png' # 保存ファイルパスを作成
+            img = Image.open(filepath)
+            img = img.convert('RGBA') # RGBA(png)→RGB(jpg)へ変換
+            img = img.rotate(i*10)
+            img.save(save_filepath, "PNG", quality=95)
+            print(filepath, '->', save_filepath)
+        if flag_delete_original_files:
+            os.remove(filepath)
+            print('delete', filepath)
 
 if __name__ == '__main__':
-
-    # 出力先ディレクトリの設定
-    output_dir = os.getcwd()
-
-    # 拡張する画像群の読み込み
-    images = glob.glob(os.path.join('./', "*.jpg"))
-
-    # 拡張する際の設定
-    generator = ImageDataGenerator(
-                    rotation_range=90, # 90°まで回転
-                    width_shift_range=0.1, # 水平方向にランダムでシフト
-                    height_shift_range=0.1, # 垂直方向にランダムでシフト
-                    channel_shift_range=50.0, # 色調をランダム変更
-                    shear_range=0.39, # 斜め方向(pi/8まで)に引っ張る
-                    horizontal_flip=True, # 垂直方向にランダムで反転
-                    vertical_flip=True # 水平方向にランダムで反転
-                    )
-
-    # 読み込んだ画像を順に拡張
-    for i in range(len(images)):
-        img = load_img(images[i])
-        # 画像を配列化して転置a
-        x = img_to_array(img)
-        x = np.expand_dims(x, axis=0)
-        # 画像の拡張
-        draw_images(generator, x, output_dir, i)
+    input_path = str(sys.argv[1]) # オリジナルpngファイルがあるフォルダを指定
+    out_path = input_path # 変換先のフォルダを指定
+    flag_delete_original_files = True # 元ファイルを削除する場合は、True指定
+    main()
